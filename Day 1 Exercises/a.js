eb5a60ffd8308eafb6532aa47d6b952d3089b6d7
Ayodeji <ayoeluyemi2002@gmail.com>
2022-08-06 20:38:48 +0100

    ww

---
+++ Day 1 Exercises/a.js
@@ -0,0 +1,48 @@
+// 1.
+// Comments can make code readable
+
+// 2.
+// Welcome to 30DaysOfJavaScript
+
+// 3.
+/**
+    comments can make readable, easy
+    to reuse and informative
+**/
+
+// 6.
+//Declare four variables without assigning values
+let nam; 
+let cal; 
+let bab; 
+let ter; 
+
+
+// 7.
+//Declare four variables with assigned values
+let errs = 40;
+let dns = "chrgtnkfcyeig"
+let hvnf = "hgnknbaysi"
+let ngj = "cdvnk vhy"
+
+
+// 8.
+//Declare variable to store firstname, lastname, marital status, country and age in multiple lines
+let firstName = "Ayor"
+let lastName = "Eluyemi"
+let maritalStatus = "Not single"
+let country = "Nigeria"
+let age = "20"
+console.log(firstName, lastName,maritalStatus,country,age)
+
+// 9.
+//Declare variable to store firstname, lastname, marital status, country and age in single lines
+let firstName = "Ayor", lastName = "Eluyemi", maritalStatus = "Not single", country = "Nigeria", age = "20";
+
+
+// 10.
+//Declare two variables myAge and yourAge 
+let myAge = 20;
+let yourAge = 21;
+console.log(myAge);
+console.log(yourAge);
---
+++ Day 1 Exercises/variable.js
@@ -0,0 +1,17 @@
+// Create a variable and declare variables and assign string, boolean, undefined and null datatyped
+let man;
+
+let Name = "Ayor" // string
+
+
+let nw = 22;
+let cat = 12;
+console.log(a>b);
+
+
+const name = null;
+var a = "12"
+var b = "12"
+console.log(a * b)
+
+
